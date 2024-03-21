b805211fbb8f02d9ebddeee345fe07c00b201409
Theodoraldo <theodoraldo@gmail.com>
2024-03-21 21:12:36 +0000

    Add car route resources

--- app/Http/Controllers/Api/V1/BookingController.php
+++ app/Http/Controllers/Api/V1/BookingController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Api\V1\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class BookingController extends Controller
 {
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Car;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('car', Car::class);
