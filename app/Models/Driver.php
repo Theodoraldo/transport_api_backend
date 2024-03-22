c0d8fa812d6a86e73e6d3d6ba53e4296a96f650c
Theodoraldo <theodoraldo@gmail.com>
2024-03-22 13:21:36 +0000

    Add accessor and mutator for fullname

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -10,7 +10,14 @@ class Driver extends Model
 {
     use HasFactory;
 
-    public $fillable = ['firstname', 'lastname', 'phone_number', 'address'];
+    public $fillable = ['firstname', 'lastname', 'phone_number', 'identity_type', 'identity_number', 'address'];
+
+    protected $appends = ['full_name'];
+
+    public function getFullNameAttribute() : string
+    {
+        return $this->firstname . ' ' . $this->lastname;
+    }
 
     public function tripInfo() : HasMany
     {
