457365caa4ee44be71a987441038b84316b7f935
Theodoraldo <theodoraldo@gmail.com>
2024-03-26 07:27:24 +0000

    Added group routes

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Car;
+use App\Http\Controllers\Api\V1\CarController;
+use App\Http\Controllers\Api\V1\DriverController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('car', Car::class);
+Route::prefix('v1')->group(function() 
+{
+    Route::apiResource('car', CarController::class);
+    Route::apiResource('driver', DriverController::class);
+});
